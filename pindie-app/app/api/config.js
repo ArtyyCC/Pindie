16b83d4d3d12e2327a0c003d2489fbf42172adcb
Artyom Medvedev <artyycc@yandex.com>
2024-05-16 16:52:05 +0300

    fixs

--- pindie-app/app/api/config.js
+++ pindie-app/app/api/config.js
@@ -2,7 +2,7 @@ export const BASE_URL = 'http://localhost:3001/api';
 
 export const endpoints = {
     games: `${BASE_URL}/games`,
-    auth: `${BASE_URL}/auth/local`,
+    auth: `${BASE_URL}/auth/login`,
     me: `${BASE_URL}/users/me`,
     register: `${BASE_URL}/auth/local/register`
 };
